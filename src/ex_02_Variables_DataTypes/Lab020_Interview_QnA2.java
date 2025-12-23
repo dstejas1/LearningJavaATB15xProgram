237ee077b756b88d6a356d87b6209b7aef119e82
Tejas DS <dstejas1@gmail.com>
2025-12-23 20:50:31 +0530

    Added Operators package

---
+++ src/ex_02_Variables_DataTypes/Lab020_Interview_QnA2.java
@@ -0,0 +1,13 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab020_Interview_QnA2 {
+    static void main(String[] args) {
+        //int enum = 10;
+        int Enum = 10;
+        System.out.println(Enum);
+        int Tejas$ = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+        System.out.println(Tejas$);
+    }
+}
---
+++ src/ex_04_Operators/Lab037_Arithmetic_Operators.java
@@ -0,0 +1,19 @@
+package ex_04_Operators;
+
+public class Lab037_Arithmetic_Operators {
+    static void main(String[] args) {
+        // Arithmetic Operators
+        //        + (Addition)
+        //       - (Subtraction)
+        //       * (Multiplication)
+        //       / (Division)
+        //       % (Modulus) | Modulus - Remainder
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(a%b);
+    }
+}
